3ade09f96680cda1c39c443c9a885aa156ad1fc1
Maxim Vysotskiy <75042447+mavrus76@users.noreply.github.com>
2022-07-05 13:22:49 +0300

    rename project

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ cd dist
 git init
 git add .
 git commit -m 'deploy'
-git push -f git@github.com:mavrus76/vue-tehnozavr.git master:gh-pages
+git push -f git@github.com:mavrus76/vue-tehnozavr-dist.git master:gh-pages
 
 cd ~
